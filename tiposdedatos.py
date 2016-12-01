05dc3c2b5ccac2ca3f7a0f197aaabbfa46a67683
Jhonbeltran <jbeltranleon@gmail.com>
2016-11-30 19:22:12 -0500

    Agrego primer archivo con tipos de datos

---
+++ tiposdedatos.py
@@ -0,0 +1,32 @@
+# Entero
+edad = 3**2
+
+# Podemos definir una variable vacia de la siguiente forma
+vacia = int()
+
+# De punto flotante float
+altura = 15.04
+
+# Enteros enormes long 
+cromosomas = 50L
+
+# Booleano bool 
+domestico = True
+
+# Cadena de caracteres (string) str 
+especie = ("Felis silvestris catus")
+
+# Listas, algo asi como colecciones de diferentes datos list 
+aleatorio = [1, 3, [45, "Algo"], 4.5]
+mascota = [edad, altura, cromosomas, domestico, especie]
+
+print mascota
+
+# Diccionario de datos dict 
+propietario = {'nombre' : 'Jhon'}
+
+# Tupla que es una lista que no se puede editar tuple
+random = ('a', 1, 3.4)
+
+""" Esta vaina es una
+Anotacion o comentario """
