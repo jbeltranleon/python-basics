905e7358e80c4748122a7ab257a19e76ab4b1612
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-05 23:59:39 -0500

    Agrego ejercicio de allar palindromo en python

--- strings.py
+++ strings.py
@@ -15,12 +15,23 @@ def main(text):
 	print(methods)
 
 	slices(text)
+	palindrome(text)
 
 def slices(text):
 	print('\nEntramos a la función de rebanadas')
 	print()
-	ways = [text[1:]]
+	ways = [text[1:], text[1:5], text[1:5:2], text[::-1]]
 	print(ways)
+	print('Para la ultima es de donde a donde y de cuanto en cuanto')
+
+def palindrome(text):
+	print('\nVamos a averiguar si la palabra es un palindromo')
+	without_spaces_text = text.replace(' ', '')
+
+	if without_spaces_text == without_spaces_text[::-1]:
+		print('La cadena {} SI es un palindromo'.format(text))
+	else:
+		print('La cadena {} no es un palindromo'.format(text))
 
 if __name__ == '__main__':
 	text = input('Ingresa una string: ')
