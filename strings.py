af1b4958d2edc7a450b17549a6ef2c5e131815b4
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-06 09:58:22 -0500

    Agrego colores a los outputs usando la libreria colorama $ pip install colorama

--- strings.py
+++ strings.py
@@ -1,5 +1,9 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
 def main(text):
-	print(text)
+	print(Fore.MAGENTA+'\nEl string que ingresaste es: {}'.format(text))
 
 	methods = [ text.upper(),
 				text.isupper(),
@@ -16,22 +20,27 @@ def main(text):
 
 	slices(text)
 	palindrome(text)
+	change_word(text)
 
 def slices(text):
-	print('\nEntramos a la función de rebanadas')
+	print(Fore.RED+'\nEntramos a la función de rebanadas')
 	print()
 	ways = [text[1:], text[1:5], text[1:5:2], text[::-1]]
 	print(ways)
 	print('Para la ultima es de donde a donde y de cuanto en cuanto')
 
 def palindrome(text):
-	print('\nVamos a averiguar si la palabra es un palindromo')
+	print(Fore.CYAN+'\nVamos a averiguar si la palabra es un palindromo')
 	without_spaces_text = text.replace(' ', '')
 
 	if without_spaces_text == without_spaces_text[::-1]:
 		print('La cadena {} SI es un palindromo'.format(text))
 	else:
 		print('La cadena {} no es un palindromo'.format(text))
+
+def change_word(text):
+	new_string = 'L' + text[1:]
+	print(Fore.GREEN+'\nLa nueva string es: {}'.format(new_string))
 
 if __name__ == '__main__':
 	text = input('Ingresa una string: ')
