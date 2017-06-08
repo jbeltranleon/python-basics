112827cf81252b010d32f7bf6bcef48f77f1d12e
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-08 12:43:45 -0500

    Agrego ejemplo incompleto de cronometro de vueltas

--- list.py
+++ list.py
@@ -1,11 +1,22 @@
-favorites = list()
+from colorama import init, Back
+from datetime import datetime
+from curtsies import Input
 
-favorites.append('Python')
-favorites.append('English')
-favorites.append('Work out')
-print('Sí imprimo toda la lista: ')
-print(favorites)
+def start_stop():
+    with Input(keynames='curses') as input_generator:
+        for e in input_generator:
+            if e == '\n':
+                print('Stop')
+            else:
+                print(repr('Start/Stop'))
+                store_time()
 
-print('Si la imprimo usando un ciclo: ')
-for favorite in favorites:
-	print(favorite)
+def store_time():
+    times = list()
+    times.append(datetime.now())
+    print(times[0])
+
+
+if __name__ == '__main__':
+    print('Para cancelar presiona Ctrl c')
+    start_stop()
