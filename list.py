469b8fba909ffffe3cdd67b1f75307b5bc0fb80f
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-08 12:15:39 -0500

    Agrego ejemplo basico de listas

---
+++ list.py
@@ -0,0 +1,11 @@
+favorites = list()
+
+favorites.append('Python')
+favorites.append('English')
+favorites.append('Work out')
+print('Sí imprimo toda la lista: ')
+print(favorites)
+
+print('Si la imprimo usando un ciclo: ')
+for favorite in favorites:
+	print(favorite)
