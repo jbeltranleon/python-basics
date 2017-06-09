4643dd15658f3b8afec3a3b116280d05ce6cf48a
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-08 22:46:08 -0500

    Agrego ejemplo de promedio de temperaturas

--- list.py
+++ list.py
@@ -1,24 +1,14 @@
-from colorama import init, Back
-from datetime import datetime, time
-from curtsies import Input
+def average_temps(temps):
+	sum_of_temps = 0
 
-def start_stop():
-    times = list()
-    with Input(keynames='curses') as input_generator:
-        for e in input_generator:
-            if e == '\n':
-                break
-            else:
-                print(repr('Start/Stop'))
-                store_time(times)
+	for temp in temps:
+		sum_of_temps += temp
 
-    for time in times:
-        print(Back.BLUE+'El resultado del {} tiempo es de {}'.format(times.index(time)+1, time))
-
-def store_time(times):
-    times.append(datetime.now().time())
-
+	return sum_of_temps / len(temps)
 
 if __name__ == '__main__':
-    print('Para terminar y ver los datos presiona Enter')
-    start_stop()
+	temps = [21, 24, 24, 22, 20, 23, 24]
+
+	average = average_temps(temps)
+
+	print('La temperatura promedio es: {}'.format(average))
---
+++ list_store_time.py
@@ -0,0 +1,24 @@
+from colorama import init, Back
+from datetime import datetime, time
+from curtsies import Input
+
+def start_stop():
+    times = list()
+    with Input(keynames='curses') as input_generator:
+        for e in input_generator:
+            if e == '\n':
+                break
+            else:
+                print(repr('Start/Stop'))
+                store_time(times)
+
+    for time in times:
+        print(Back.BLUE+'El resultado del {} tiempo es de {}'.format(times.index(time)+1, time))
+
+def store_time(times):
+    times.append(datetime.now().time())
+
+
+if __name__ == '__main__':
+    print('Para terminar y ver los datos presiona Enter')
+    start_stop()
