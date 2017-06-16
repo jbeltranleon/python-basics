209690b950da29710a3aa6350aea0e31dc8d8293
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-15 23:31:30 -0500

    Agrego estructura básica del juego del ahorcado

---
+++ ahorcado.py
@@ -0,0 +1,105 @@
+import random
+
+# constante
+IMAGES = ['''
+
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        =========''', '''
+''']
+
+WORDS = [
+    'engurrupletadora',
+    'sofa',
+    'dictadura',
+    'uribe',
+    'pistola',
+    'sql'
+    'computador',
+    'vaso',
+    'vaca',
+    'ninfomana'
+]
+
+def run():
+    word = random_word()
+    hidden_word = ['-'] * len(word)
+    tries = 0
+
+    while True:
+        display_board(hidden_word, tries)
+        current_letter = str(input('Escoge una letra: '))
+
+def random_word():
+    index = random.randint(0, len(WORDS) - 1)
+    return WORDS[index]
+
+def display_board(hidden_word, tries):
+    print(IMAGES[tries])
+    print('')
+    print(hidden_word)
+    print('--- * --- * --- * --- * --- * ')
+
+if __name__ == '__main__':
+    print('¡Juega al ahorcado!')
+    run()
