ff80c012f23287889dd30d617e5bdbad25cb5c87
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-25 22:58:07 -0500

    Agrego ejemplo base de diccionario

---
+++ diccionario.py
@@ -0,0 +1,9 @@
+# Un diccionario es un mapa de llaves a valores
+# Los valores pueden ser de cualquier tipo.
+
+mi_dict = {}
+# mi_dict = dict()
+mi_dict['llave'] = 'valor'
+otro_dict = {'otra llave': 'otro valor'}
+print(mi_dict)
+print(otro_dict)
