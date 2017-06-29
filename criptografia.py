935c72b899bf6fbdc490187021d9c365bc900b7f
Jhonbeltran <jbeltranleon@gmial.com>
2017-06-28 20:26:12 -0500

    Agrego ejemplo de criptografia con funcion de encriptar

---
+++ criptografia.py
@@ -0,0 +1,117 @@
+KEYS = {
+    'a': 'w',
+    'b': 'E',
+    'c': 'x',
+    'd': '1',
+    'e': 'a',
+    'f': 't',
+    'g': '0',
+    'h': 'C',
+    'i': 'b',
+    'j': '!',
+    'k': 'z',
+    'l': '8',
+    'm': 'M',
+    'n': 'I',
+    'o': 'd',
+    'p': '.',
+    'q': 'U',
+    'r': 'Y',
+    's': 'i',
+    't': '3',
+    'u': ',',
+    'v': 'J',
+    'w': 'N',
+    'x': 'f',
+    'y': 'm',
+    'z': 'W',
+    'A': 'G',
+    'B': 'S',
+    'C': 'j',
+    'D': 'n',
+    'E': 's',
+    'F': 'Q',
+    'G': 'o',
+    'H': 'e',
+    'I': 'u',
+    'J': 'g',
+    'K': '2',
+    'L': '9',
+    'M': 'A',
+    'N': '5',
+    'O': '4',
+    'P': '?',
+    'Q': 'c',
+    'R': 'r',
+    'S': 'O',
+    'T': 'P',
+    'U': 'h',
+    'V': '6',
+    'W': 'q',
+    'X': 'H',
+    'Y': 'R',
+    'Z': 'l',
+    '0': 'k',
+    '1': '7',
+    '2': 'X',
+    '3': 'L',
+    '4': 'p',
+    '5': 'v',
+    '6': 'T',
+    '7': 'V',
+    '8': 'y',
+    '9': 'K',
+    '.': 'Z',
+    ',': 'D',
+    '?': 'F',
+    '!': 'B',
+}
+
+def cypher(message):
+    words = message.split(' ')
+    cypher_message = []
+
+    for word in words:
+        cypher_word = ''
+        for letter in word:
+            cypher_word += KEYS[letter]
+
+        cypher_message.append(cypher_word)
+
+    return ' '.join(cypher_message)
+
+
+def decipher(message):
+    pass
+
+
+def run():
+
+    while True:
+
+        command = str(input('''--- * --- * --- * --- * --- * --- * --- * ---
+
+            Bienvenido a criptografía. ¿Qué deseas hacer?
+
+            [c]ifrar mensaje
+            [d]ecifrar mensaje
+            [s]alir
+        '''))
+
+        if command == 'c':
+            message = str(input('Escribe tu mensaje: '))
+            cypher_message = cypher(message)
+            print(cypher_message)
+
+        elif command == 'd':
+            print('decifrar')
+        elif command == 's':
+            print('salir')
+            break
+        else:
+            print('¡Comando no encontrado!')
+
+
+if __name__ == '__main__':
+    print('M E N S A J E S  C I F R A D O S')
+    run()
