1585d74fb79d02067f8dbe9a4237b8067ec7a0cc
Jhonbeltran <jbeltranleon@gmail.com>
2017-07-04 00:04:56 -0500

    Agrego pertenece y no pertenece en los sets

--- sets.py
+++ sets.py
@@ -1,4 +1,12 @@
-""" Son como Conjuntos """
+""" Son como Conjuntos 
+	
+	Uno de los usos comunes con los sets es saber si un elemento pertenece a un set,
+	si queremos saber si un elemento pertenece a una lista es mucho menos eficiente
+	comparado con un set.
+	Sí tenemos un gran número de elementos y sabemos que no se repiten ó no nos
+	importa si se repiten es mucho mas eficiente realizar operaciones con sets que 
+	con listas
+"""
 
 s = set([1,2,3])
 t = set([3,4,5])
@@ -11,3 +19,9 @@ print(intersection)
 
 difference = s.difference(t)
 print(difference)
+
+# if 1 not in s:
+if 1 in s:
+	print('Pertenece')
+else:
+	print('No pertenece')
