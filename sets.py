2293c3e48f9ecf0043a96529dfcd8cf442f6eebc
Jhonbeltran <jbeltranleon@gmail.com>
2017-07-03 23:59:21 -0500

    Agrego ejemplo de sets Recién conozco este tipo de datos WUHU

---
+++ sets.py
@@ -0,0 +1,13 @@
+""" Son como Conjuntos """
+
+s = set([1,2,3])
+t = set([3,4,5])
+
+union = s.union(t)
+print(union)
+
+intersection = s.intersection(t)
+print(intersection)
+
+difference = s.difference(t)
+print(difference)
