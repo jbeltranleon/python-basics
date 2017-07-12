31bf2924f557ff42512dcca453264e85cd487561
Jhonbeltran <jbeltranleon@gmail.com>
2017-07-12 15:56:01 -0500

    paquetes y modulos

---
+++ lamp.py
@@ -0,0 +1,37 @@
+class Lamp:
+	_LAMPS = ['''
+          .
+     .    |    ,
+      \   '   /
+       ` ,-. '
+    --- (   ) ---
+         \ /
+        _|=|_
+       |_____|
+    ''',
+    '''
+         ,-.
+        (   )
+         \ /
+        _|=|_
+       |_____|
+    ''']
+
+	# __init__ = constructor
+	# self = instancia(forma de manipular la clase)
+	def __init__(self, is_turned_on):
+		self._is_turned_on = is_turned_on
+
+	def turn_on(self):
+		self._is_turned_on = True
+		self._display_image()
+
+	def turn_off(self):
+		self._is_turned_on = False
+		self._display_image()
+
+	def _display_image(self):
+		if self._is_turned_on:
+			print(self._LAMPS[0])
+		else:
+			print(self._LAMPS[1])
